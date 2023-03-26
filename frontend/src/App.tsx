4c2e1d92554b0a20ac86b00fcbf3556370da5899
smwoolar <woolard.34@osu.edu>
2023-03-25 21:41:46 -0400

    payment updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./Home/Home";
 import Products from "./Products/Products";
 import Orders from './Orders/Orders';
 import OrderDetail from './Orders/OrderDetail';
+import PaymentScreen from './Payment/Payment';
 
 function App() {
   const openMenu = () => {
@@ -30,6 +31,7 @@ function App() {
                 <Link to="/">Home</Link>
                 <Link to="/catalog">Catalog</Link>
                 <Link to="/orders">Orders</Link>
+                
             </div>
         </header>
         <aside className="sidebar">
